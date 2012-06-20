30daf6dac908312ea3b8bf55881ff39be7c3b1d6
Daniel Nill <DanielLNill@gmail.com>
2012-06-19 23:13:11 -0500

    initial addition of files

---
+++ server.js
@@ -0,0 +1,54 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var server;
+
+server = http.createServer(function(req, res){
+    // your normal server code
+    var path = url.parse(req.url).pathname;
+    switch (path){
+        case '/':
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write('<h1>Hello! Try the <a href="/test.html">Test page</a></h1>');
+            res.end();
+            break;
+        case '/test.html':
+            fs.readFile(__dirname + path, function(err, data){
+                if (err){ 
+                    return send404(res);
+                }
+                res.writeHead(200, {'Content-Type': path == 'json.js' ? 'text/javascript' : 'text/html'});
+                res.write(data, 'utf8');
+                res.end();
+            });
+        break;
+        default: send404(res);
+    }
+}),
+
+send404 = function(res){
+    res.writeHead(404);
+    res.write('404');
+    res.end();
+};
+
+server.listen(8001);
+
+// use socket.io
+var io = require('socket.io').listen(server);
+
+//turn off debug
+io.set('log level', 1);
+
+// define interactions with client
+io.sockets.on('connection', function(socket){
+    //send data to client
+    setInterval(function(){
+        socket.emit('date', {'date': new Date()});
+    }, 1000);
+
+    //recieve client data
+    socket.on('client_data', function(data){
+        process.stdout.write(data.letter);
+    });
+});
